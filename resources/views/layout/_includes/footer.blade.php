2e3236391ef3d64343a2c13fcf2b8a93d99577f2
Samuel F Duarte <samuel_fx@hotmail.com>
2018-07-07 23:23:02 -0300

    Inserindo o footer css portal vintage

--- resources/views/layout/_includes/footer.blade.php
+++ resources/views/layout/_includes/footer.blade.php
@@ -1,11 +1,56 @@
 
 
+
+  <footer class="page-footer " style="background-color: black;" >
+    <div class="container">
+
+      <div class="row" >
+        <div class="col l6 s12" style="width: 20%!important;">
+          <h6 class="white-text column-one-txt" >A product of</h6>
+          <h5 class="grey-text text-lighten-4">Vintage</h5>
+          <h6 class="grey-text text-lighten-4 column-one-txt">Collection</h6>
+        </div>
+
+        <div class="col l6 s12" align="center" style="width: 20%!important;" >
+          <h6 class="grey-text text-lighten-4">ABOUT</h6>
+          <h6 class="grey-text text-lighten-4 column-one-txt">ABOUT US</h6>
+          <h6 class="white-text column-one-txt" >OUR MISSION</h6>
+          <h6 class="white-text column-one-txt" >REVIEWS</h6>
+        </div>
+
+        <div class="col l6 s12"  align="center" style="width: 20%!important;" >
+          <h6 class="grey-text text-lighten-4">MENU</h6>
+          <h6 class="grey-text text-lighten-4 column-one-txt">FOR MEN</h6>
+          <h6 class="white-text column-one-txt" >FOR WOMAN</h6>
+          <h6 class="white-text column-one-txt" >OTHERS</h6>
+        </div>
+
+        <div class="col l4 offset-l2 s12" align="center" style="width: 20%!important;" >
+          <ul>
+            <h6 class="white-text topo-options-txt-footer">SOCIAL MEDIA</h6>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+    <div class="footer-copyright">
+      <h6 class="white-text column-one-txt" style="margin-left:15.8%!important;">
+            support@vicenrowhatches.com
+      </h6>
+      <div class="container" style="margin-left:43%!important;">
+           <a class="grey-text text-lighten-4 column-one-txt" href="#!">
+               2018 Vintage - All rights reserved.
+            </a>
+      </div>
+    </div>
+  </footer>
+
   <!--JavaScript at end of body for optimized loading-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
   <script type="text/javascript">
 
-      $(document).ready(function(){      
+      $(document).ready(function(){
         $('.sidenav').sidenav();
       });
 
