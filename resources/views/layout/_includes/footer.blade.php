bf7cfe66475e55b731f5c5dc9cb0afd46c9ddf68
Samuel F Duarte <samuel_fx@hotmail.com>
2018-07-08 09:47:22 -0300

    Updated footer page, email block insert

--- resources/views/layout/_includes/footer.blade.php
+++ resources/views/layout/_includes/footer.blade.php
@@ -1,6 +1,15 @@
 
-
-
+  <div align="center"  >
+    <h3 class="txt-header-description-footer-email" >WANT 80% OFF?</h3>
+    <h5>SUBSCRIBE BELOW TO GET</h5>
+    <input id="email" class="input-search email-footer-input"
+           type="search" placeholder="Email" >
+    <br/>
+    <br/>
+    <button type="button" class="btn-footer-subscribe" >SUBSCRIBE</button>
+  </div>
+  <br/>
+  <br/>
   <footer class="page-footer footer-tag" >
     <div class="container">
 
